dc88336a183b5f85ff1cc0b11439efbdf4c063e9
Jose Ortega <jose.ort98@gmail.com>
2022-06-01 12:14:56 -0500

    Ejercicios 1, 2, 3, 4 y 5

--- taller.py
+++ taller.py
@@ -11,3 +11,19 @@ data['Nombre departamento'].replace(
 data.Sexo.replace(['m', 'f'], ['M', 'F'], inplace=True)
 data['Ubicación del caso'].replace(
     ['CASA', 'casa'], ['Casa', 'Casa'], inplace=True)
+
+
+# Ejercicio 1
+print(data['ID de caso'].count())
+
+# Ejercicio 2
+print(data['Nombre municipio'].unique().size)
+
+# Ejercicio 3
+print(data['Nombre municipio'].unique())
+
+# Ejercicio 4
+print(len(data[data['Ubicación del caso'] == 'Casa']))
+
+# Ejercicio 5
+print(len(data[data['Recuperado'] == 'Recuperado']))
